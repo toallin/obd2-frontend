436df6245f5069192319f69a8121f69fac827067
cristian <crisquispel@upt.pe>
2026-06-04 22:21:19 -0500

    2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Esto obliga a Vite a usar la variable de Render, y si no existe en el entorno, usa la de producción directamente
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://obd2-backend.onrender.com/api')
-  }
+  base: "./" // Esto asegura que Render encuentre la carpeta assets sin importar la ruta
 })
