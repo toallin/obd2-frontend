341eadd8733669755ce5c2f5dbef2586d47b8b7f
cristian <crisquispel@upt.pe>
2026-06-07 19:26:41 -0500

    delete

--- README.md
+++
@@ -1,1 +0,0 @@
-# obd2-frontend
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/" // Asegura que las rutas y assets se resuelvan desde la raíz para evitar errores 404 al recargar
-})
