bba24a055c7a5ac756628793716dcc475a527b4c
cristian <crisquispel@upt.pe>
2026-06-07 19:22:47 -0500

    render2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./" // Esto asegura que Render encuentre la carpeta assets sin importar la ruta
+  base: "/" // Asegura que las rutas y assets se resuelvan desde la raíz para evitar errores 404 al recargar
 })
